683e169f448134b87523cbd80bccdb50365ecf64
darshilRK <darshilkothari1717@gmail.com>
2023-06-08 12:22:44 +0530

    data fetched from user and mongodb atlas

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
-const mongoURI ='mongodb+srv://gofood:darsh@cluster0.t9h1vmr.mongodb.net/?retryWrites=true&w=majority'
+const mongoURI ='mongodb+srv://gofood:darsh@cluster0.t9h1vmr.mongodb.net/gofood?retryWrites=true&w=majority'
 const mongoDB= async() =>{
     await mongoose.connect(mongoURI,{ useNewUrlParser: true},(err,result)=>{
         if(err){
-            console.log("---",err)
+            console.log("---",err);
         }else{
-            console.log("connected")
+            console.log("connected");
+            const fetched_data = mongoose.connection.db.collection("food_items");
+            fetched_data.find({}).toArray(function(err,data){
+                if(err){
+                    console.log("---",err);
+                }else{
+                    //console.log(data);
+                }
+            })
         } 
     });
 }
