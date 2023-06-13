a0e973c2a68bc2d8625e63e0916bc695ec1dab9f
darshilRK <darshilkothari1717@gmail.com>
2023-06-13 17:26:44 +0530

    data display on frontend

--- backend/db.js
+++ backend/db.js
@@ -1,21 +1,30 @@
 const mongoose = require('mongoose');
-const mongoURI ='mongodb+srv://gofood:darsh@cluster0.t9h1vmr.mongodb.net/gofood?retryWrites=true&w=majority'
-const mongoDB= async() =>{
-    await mongoose.connect(mongoURI,{ useNewUrlParser: true},(err,result)=>{
-        if(err){
-            console.log("---",err);
-        }else{
+const mongoURI = 'mongodb+srv://gofood:darsh@cluster0.t9h1vmr.mongodb.net/gofood?retryWrites=true&w=majority'
+const mongoDB = async () => {
+    await mongoose.connect(mongoURI, { useNewUrlParser: true }, (err, result) => {
+        if (err) {
+            console.log("---", err);
+        } else {
             console.log("connected");
             const fetched_data = mongoose.connection.db.collection("food_items");
-            fetched_data.find({}).toArray(function(err,data){
-                if(err){
-                    console.log("---",err);
-                }else{
-                    //console.log(data);
-                }
+            fetched_data.find({}).toArray(async function (err, data) {
+                const foodCategory = await mongoose.connection.db.collection("foodCategory");
+                foodCategory.find({}).toArray(function (err, catData) {
+                    if (err) {
+                        console.log("---", err);
+                    } else {
+                        global.food_items = data;
+                        global.foodCategory = catData;
+                    }
+                })
+                // if(err){
+                //     console.log("---",err);
+                // }else{
+                //     global.food_items = data;
+                // }
             })
-        } 
+        }
     });
 }
 
-module.exports=mongoDB;
+module.exports = mongoDB;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
-export default function Card() {
+export default function Card(props) {
+
+    let options = props.options;
+    let priceOption = Object.keys(options)
+
     return (
         <div>
             <div>
                 <div className="card mt-3" style={{ "width": "18rem", "maxHeight": "360px" }}>
-                    <img src="https://www.yummytummyaarthi.com/wp-content/uploads/2014/09/1-27.jpg" className="card-img-top" alt="..." />
+                    <img src={props.imgSrc} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
+                        <h5 className="card-title">{props.foodName}</h5>
                         <p className="card-text">this is some important text.</p>
                         <div className="container w-100">
                             <select className="m-2 h-100 bg-success rounded">
@@ -18,8 +22,11 @@ export default function Card() {
                                 })}
                             </select>
                             <select className="m-2 h-100 bg-success rounded">
-                                <option value="half">Half</option>
-                                <option value="full">Full</option>
+                                {
+                                    priceOption.map((data) => {
+                                        return <option key={data} value={data}>{data}</option>
+                                    })
+                                }
                             </select>
                             <div className='d-inline h-100 fs-5'>
                                 total price
